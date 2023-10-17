34e21dd1b20b0aa62ab2fa2f20fead64c5a9c555
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-10-17 20:43:18 +0200

    Add item model

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +15,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDb")));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
