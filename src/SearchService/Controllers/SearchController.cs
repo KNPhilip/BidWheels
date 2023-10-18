be6daab4e0f26add417f8aa13d8021bfde0015b9
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-10-18 17:46:54 +0200

    Add ordering

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
+using ZstdSharp.Unsafe;
 
 namespace SearchService.Controllers
 {
@@ -9,17 +11,23 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
         {
-            var query = DB.PagedSearch<Item>();
+            var query = DB.PagedSearch<Item, Item>();
 
-            query.Sort(x => x.Ascending(a => a.Make));
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
 
-            if (!string.IsNullOrEmpty(searchTerm))
-                query.Match(Search.Full, searchTerm).SortByTextScore();
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+                // Underscore is the default ordering option.
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            };
 
-            query.PageNumber(pageNumber);
-            query.PageSize(pageSize);
+            query.PageNumber(searchParams.PageNumber);
+            query.PageSize(searchParams.PageSize);
 
             var result = await query.ExecuteAsync();
 
