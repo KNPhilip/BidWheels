2f8c0fe30027ac7431d4086370d54d49bac39351
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-10-19 12:15:29 +0200

    Add http communication to get auction data

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -1,7 +1,7 @@
-using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data
 {
@@ -18,19 +18,17 @@ namespace SearchService.Data
                 .Key(x => x.Color, KeyType.Text)
                 .CreateAsync();
 
-            var count = await DB.CountAsync<Item>();
+            Int64 count = await DB.CountAsync<Item>();
 
-            if (count == 0)
-            {
-                Console.WriteLine("--> No data. Attempting to seed..");
-                var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            using var scope = app.Services.CreateScope();
 
-                var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+            AuctionServiceHttpClient httpClient = scope.ServiceProvider.GetRequiredService<AuctionServiceHttpClient>();
 
-                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            List<Item> items = await httpClient.GetItemsForSearchDb();
 
-                await DB.SaveAsync(items!);
-            }
+            Console.WriteLine($"--> {items.Count} returned from the auction service");
+
+            if (items.Count > 0) await DB.SaveAsync(items);
         }
     }
 }
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,10 +1,12 @@
 using SearchService.Data;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
