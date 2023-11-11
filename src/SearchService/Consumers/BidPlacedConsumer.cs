e5be4dbfaea34791b7e1288e1a4a971f8111907e
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-11-11 10:02:07 +0100

    Add remaining consumers in SearchService

---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,24 @@
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Contracts;
+using SearchService.Models;
+
+namespace SearchService.Consumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("--> Consuming bid placed");
+
+            Item? auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            if (context.Message.BidStatus!.Contains("Accepted") 
+                && context.Message.Amount > auction!.CurrentHighBid)
+            {
+                auction.CurrentHighBid = context.Message.Amount;
+                await auction.SaveAsync();
+            }
+        }
+    }
+}
