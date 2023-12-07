ff1f48d13ca4827e2eb37f81a83084781cb2d48c
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-12-07 20:39:02 +0100

    Add navbar

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navbar from './nav/Navbar'
 
 export const metadata: Metadata = {
   title: 'BidWheels',
@@ -13,7 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main className="container mx-auto px-5 pt-10">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
