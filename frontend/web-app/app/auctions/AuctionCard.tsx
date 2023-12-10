a1f860c64fb50c223cad73924380678eb542e304
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-12-10 11:46:50 +0100

    Add countdown timer to card

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import CountdownTimer from './CountdownTimer'
 
 type Props = {
     auction: any
@@ -18,6 +19,9 @@ const AuctionCard = ({auction}: Props) => {
                     className="object-cover"
                     sizes='(max-width:768px) 100vw, (max-width: 1200px) 50vw, 25vw'
                 />
+                <div className="absolute bottom-2 left-2">
+                    <CountdownTimer auctionEnd={auction.auctionEnd} />
+                </div>
             </div>
             <div className="flex justify-between items-center mt-4">
                 <h3 className="text-gray-700">{auction.make} {auction.model}</h3>
