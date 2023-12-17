579c5b1118a1bfb0c8340df61a144969d06f1eb6
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-12-17 09:27:17 +0100

    Add search functionality

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+'use client'
+
+import { useParamStore } from '@/hooks/useParamsStore'
+import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
 const Search = () => {
+    const setParams = useParamStore(state => state.setParams);
+    const [value, setValue] = useState('');
+
+    function onChange(event: any) {
+        setValue(event.target.value);
+    }
+
+    function search() {
+        setParams({searchTerm: value});
+    }
+
     return (
         <div className="flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm">
             <input
+                onKeyDown={(e: any) => {
+                    if (e.key === 'Enter') search();
+                }}
+                onChange={onChange}
                 type="text"
                 placeholder="Search for cars by make, model or color"
                 className="
@@ -19,7 +37,7 @@ const Search = () => {
                     text-gray-600
                 "
             >
-            <button>
+            <button onClick={search}>
                 <FaSearch 
                     size={34} 
                     className="bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2"
