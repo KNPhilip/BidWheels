2bb8c4f4c505edabf9af3bfee164910b78218fc0
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-12-19 16:44:12 +0100

    Add component for no results

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { useParamStore } from '@/hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 const Listings = () => {
     const [data, setData] = useState<PagedResult<Auction>>();
@@ -37,14 +38,20 @@ const Listings = () => {
     return (
         <>
             <Filters />
-            <div className="grid grid-cols-4 gap-6">
-                {data.result.map(auction => (
-                    <AuctionCard auction={auction} key={auction.id} />
-                ))}
-            </div>
-            <div className="flex justify-center mt-4">
-                <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber} />
-            </div>
+            {data.totalCount === 0 ? (
+                <EmptyFilter showReset />
+            ) : (
+                <>
+                    <div className="grid grid-cols-4 gap-6">
+                        {data.result.map(auction => (
+                            <AuctionCard auction={auction} key={auction.id} />
+                            ))}
+                    </div>
+                    <div className="flex justify-center mt-4">
+                        <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber} />
+                    </div>
+                </>
+            )}
         </>
     )
 }
