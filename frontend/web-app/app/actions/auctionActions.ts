77690451d7ceaa87d1bc559a0874165590651f45
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-12-28 16:27:57 +0100

    Add button to test authentication.

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -9,3 +9,19 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
 
     return res.json();
 }
+
+export async function UpdateAuctionTest() {
+    const data = {
+        mileage: Math.floor(Math.random() * 100000) + 1,
+    }
+
+    const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', {
+        method: 'PUT',
+        headers: {},
+        body: JSON.stringify(data)
+    });
+
+    if (res.ok) return {status: res.status, message: res.statusText}
+
+    return res.statusText;
+}
--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getSession } from '../actions/authActions';
 import Heading from '../components/Heading';
+import AuthTest from './AuthTest';
 
 const Session = async () => {
     const session = await getSession(); 
@@ -12,6 +13,9 @@ const Session = async () => {
             <div className="bg-blue-200 border-2 border-blue-500">
                 <h3 className="text-lg">Session data</h3>
                 <pre>{JSON.stringify(session, null, 2)}</pre>
+            </div>
+            <div className='mt-4'>
+                <AuthTest />
             </div>
         </div>
     )
