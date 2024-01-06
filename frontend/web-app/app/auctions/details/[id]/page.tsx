ef064aa2412d64bf9bdac3be3bd0ffd050cc9ef4
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-01-06 11:37:34 +0100

    Create auction details page

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,7 +1,35 @@
+import { getDetailedViewData } from '@/app/actions/auctionActions'
+import Heading from '@/app/components/Heading';
 import React from 'react'
+import CountdownTimer from '../../CountdownTimer';
+import CarImage from '../../CarImage';
 
-export default function Details({params}: {params: {id: string}}) {
+export default async function Details({params}: {params: {id: string}}) {
+    const data = await getDetailedViewData(params.id);
+    
     return (
-        <div>Details for {params.id}</div>
+        <div>
+            <div className="flex justify-between">
+                <Heading title={`${data.make} ${data.model}`} />
+                <div className="flex gap-3">
+                    <h3 className="text-2xl font-semibold">Time remaining:</h3>
+                    <CountdownTimer auctionEnd={data.auctionEnd} />
+                </div>
+            </div>
+            
+            <div className="grid grid-cols-2 gap-6 mt-3">
+                <div className="w-full bg-gray-200 aspect-h-10 aspect-w-16 rounded-lg overflow-hidden">
+                    <CarImage imageUrl={data.imageUrl} />
+                </div>
+
+                <div className="border-2 rounded-lg p-2 bg-gray-100">
+                    <Heading title="Bids" />
+                </div>
+            </div>
+
+            <div className="mt-3 grid grid-cols-1 rounded-lg">
+                Auction details
+            </div>
+        </div>
     )
 }
