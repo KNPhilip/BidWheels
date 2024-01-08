bd79dac4bf730d28d0fb66902834154671d58cab
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-01-08 15:58:56 +0100

    Create delete auction

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -30,3 +30,7 @@ export async function updateAuction(data: FieldValues, id: string) {
     revalidatePath(`/auctions/${id}`);
     return res;
 }
+
+export async function deleteAuction(id: string) {
+    return await fetchWrapper.del(`auctions/${id}`);
+}
--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -6,6 +6,7 @@ import CarImage from '../../CarImage';
 import DetailedSpecs from './DetailedSpecs';
 import { getCurrentUser } from '@/app/actions/authActions';
 import EditButton from './EditButton';
+import DeleteButton from './DeleteButton';
 
 export default async function Details({params}: {params: {id: string}}) {
     const data = await getDetailedViewData(params.id);
@@ -17,7 +18,10 @@ export default async function Details({params}: {params: {id: string}}) {
                 <div className="flex items-center gap-3">
                     <Heading title={`${data.make} ${data.model}`} />
                     {user?.username === data.seller && (
-                        <EditButton id={data.id} />
+                        <>
+                            <EditButton id={data.id} />
+                            <DeleteButton id={data.id} />
+                        </>
                     )}
                 </div>
                 <div className="flex gap-3">
