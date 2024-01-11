8fe0421a8eb3a5f04a6e3e686aec8a44ba3e7126
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-01-11 20:51:28 +0100

    Configure MassTransit, Auth & DB

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -1,6 +1,34 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddMassTransit(config => 
+{
+    config.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+
+    config.UsingRabbitMq((context, cfg) => 
+    {
+        cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", host => 
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
 
 WebApplication app = builder.Build();
 
@@ -9,5 +37,8 @@ WebApplication app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("BidDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
 
 app.Run();
