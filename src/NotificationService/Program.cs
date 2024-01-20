b7187e78ab5969b1beed3148dbb8731e88ae62df
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-01-20 17:59:26 +0100

    Clean up NotificationService

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,6 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
 
 app.Run();
