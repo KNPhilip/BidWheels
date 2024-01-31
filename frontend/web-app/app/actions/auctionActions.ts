e627a6254d800ece488b0b346293cd1d3f3dc981
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-01-31 17:14:58 +0100

    Get bids for an auction

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { Auction, PagedResult } from "@/types";
+import { Auction, Bid, PagedResult } from "@/types";
 import { fetchWrapper } from "@/lib/fetchWrapper";
 import { FieldValues } from "react-hook-form";
 import { revalidatePath } from "next/cache";
@@ -33,4 +33,8 @@ export async function updateAuction(data: FieldValues, id: string) {
 
 export async function deleteAuction(id: string) {
     return await fetchWrapper.del(`auctions/${id}`);
+}
+
+export async function getBidsForAuction(id: string): Promise<Bid[]> {
+    return await fetchWrapper.get(`bids/${id}`);
 }
--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getDetailedViewData } from '@/app/actions/auctionActions'
+import { getBidsForAuction, getDetailedViewData } from '@/app/actions/auctionActions'
 import Heading from '@/app/components/Heading';
 import React from 'react'
 import CountdownTimer from '../../CountdownTimer';
@@ -11,6 +11,7 @@ import DeleteButton from './DeleteButton';
 export default async function Details({params}: {params: {id: string}}) {
     const data = await getDetailedViewData(params.id);
     const user = await getCurrentUser();
+    const bids = await getBidsForAuction(params.id);
     
     return (
         <div>
@@ -37,6 +38,9 @@ export default async function Details({params}: {params: {id: string}}) {
 
                 <div className="border-2 rounded-lg p-2 bg-gray-100">
                     <Heading title="Bids" />
+                    {bids.map(bid => (
+                        <p key={bid.id}>{bid.bidder} - ${bid.amount}</p>
+                    ))}
                 </div>
             </div>
 
