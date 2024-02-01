98b2c99e686262c326e9063f59b0b7a1ec469c8d
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-02-01 20:32:34 +0100

    Create bid store

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -8,6 +8,7 @@ import { getCurrentUser } from '@/app/actions/authActions';
 import EditButton from './EditButton';
 import DeleteButton from './DeleteButton';
 import BidItem from './BidItem';
+import BidList from './BidList';
 
 export default async function Details({params}: {params: {id: string}}) {
     const data = await getDetailedViewData(params.id);
@@ -37,12 +38,7 @@ export default async function Details({params}: {params: {id: string}}) {
                     <CarImage imageUrl={data.imageUrl} />
                 </div>
 
-                <div className="border-2 rounded-lg p-2 bg-gray-100">
-                    <Heading title="Bids" />
-                    {bids.map(bid => (
-                        <BidItem key={bid.id} bid={bid} />
-                    ))}
-                </div>
+                <BidList user={user} auction={data} />
             </div>
 
             <div className="mt-3 grid grid-cols-1 rounded-lg">
