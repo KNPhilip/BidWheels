3863d7d930688c888208b9632618da470cc74360
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-02-17 10:30:17 +0100

    Set up web app factory

--- tests/SearchService.IntegrationTests/GlobalUsings.cs
+++ tests/SearchService.IntegrationTests/GlobalUsings.cs
@@ -1,1 +1,9 @@
 global using Xunit;
+global using MassTransit;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.AspNetCore.TestHost;
+global using Mongo2Go;
+global using MongoDB.Driver;
+global using MongoDB.Entities;
+global using SearchService.Models;
